4cb2a2a18a6776c5fd01df6a66a43f3c59a72957
Igor Braga <higorb1@gmail.com>
2021-01-26 20:53:17 -0800

    Parentheses
    Signed-off-by: Igor Braga <higorb1@gmail.com>

--- gc/structs/ForwardedHeader.hpp
+++ gc/structs/ForwardedHeader.hpp
@@ -44,7 +44,7 @@
 #define OMR_BEING_COPIED_HINT 2
 #define OMR_SELF_FORWARDED_TAG J9_GC_MULTI_SLOT_HOLE
 /* combine OMR_FORWARDED_TAG with OMR_BEING_COPIED_HINT and OMR_SELF_FORWARDED_TAG into one mask which should be stripped from the pointer in order to remove all tags */
-#define OMR_FORWARDED_TAG_MASK (OMR_FORWARDED_TAG | (OMR_BEING_COPIED_HINT | OMR_SELF_FORWARDED_TAG))
+#define OMR_FORWARDED_TAG_MASK ((OMR_FORWARDED_TAG) | ((OMR_BEING_COPIED_HINT) | (OMR_SELF_FORWARDED_TAG)))
 
 
 /* Destination object header bits, masks, consts... */
